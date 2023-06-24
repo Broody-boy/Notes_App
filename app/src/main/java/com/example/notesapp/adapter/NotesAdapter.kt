0a13eaae8a0c3f1ac5ebaa57ec0fe33a66027aa2
Arshdeep <arshdeep2875@gmail.com>
2023-06-24 06:35:13 +0530

    (minor) added feature: Show image in the note card in the Home Screen as well(updated the adapter onBindViewHolder for acheiving this)

--- app/src/main/java/com/example/notesapp/adapter/NotesAdapter.kt
+++ app/src/main/java/com/example/notesapp/adapter/NotesAdapter.kt
@@ -1,9 +1,11 @@
 package com.example.notesapp.adapter
 
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
@@ -32,6 +34,14 @@ class NotesAdapter(val arrList : List<Notes>) : RecyclerView.Adapter<NotesAdapte
         else{
             holder.itemView.findViewById<CardView>(R.id.cardView).setCardBackgroundColor(ContextCompat.getColor(holder.itemView.context,R.color.ColorLightBlack))
         }
+
+        if(arrList[position].imgPath != null) {
+            holder.itemView.findViewById<ImageView>(R.id.imgNote).setImageBitmap(BitmapFactory.decodeFile(arrList[position].imgPath))
+            holder.itemView.findViewById<ImageView>(R.id.imgNote).visibility = View.VISIBLE
+        }else {
+            holder.itemView.findViewById<ImageView>(R.id.imgNote).visibility = View.GONE
+        }
+
     }
 
     class NotesViewHolder(view:View) : RecyclerView.ViewHolder(view) {
